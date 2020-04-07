8f769b80f7eccf33abb1fb4751e4b3225c079966
Maxim <max28.ozymand@gmail.com>
2020-04-07 12:11:56 +0300

    Task_1 version with only one company(input)

--- simple_task_1.py
+++ simple_task_1.py
@@ -1,48 +1,58 @@
-from bs4 import BeautifulSoup as Bs
-import requests
 import re
 import string as st
+from random import choice
+from csv import writer
+
+import requests
+from bs4 import BeautifulSoup as Bs
+from openpyxl import Workbook
 
 from for_task_1 import *
 
-from random import choice
-from openpyxl import Workbook
-from csv import writer
 
-params_company[0] = "Характеристики компании"
+PARAMS_COMPANY[0] = "Характеристики компании"
 while True:
     inp_url = input().lower()
     if re.match(r'\d+', inp_url):
         break
 
-head_cells = ["{}{}".format('A', numb+1) for numb in range(len(params_company))]
+		
+HEAD_CELLS = ["{}{}".format('A', numb+1) 
+	for numb in range(len(PARAMS_COMPANY))]
 wb = Workbook()
 ws = wb.active
-for numb, cell in enumerate(head_cells):
-    ws[cell] = params_company[numb]
+for numb, cell in enumerate(HEAD_CELLS):
+    ws[cell] = PARAMS_COMPANY[numb]
 url = '{}{}'.format(main_url, inp_url)
+
 for i in range(20):
-    useragent = {'User-agent': choice(useragents)}
+    USERAGENT = {'User-agent': choice(USERAGENTS)}
     try:
-        req = requests.get(url, headers=useragent)
+        req = requests.get(url, headers=USERAGENT)
     except:
         pass
     else:
         break
 
+		
 bs = Bs(req.text, "html.parser")
 ans = get_params(bs)
-for n, param in enumerate(params_company[1:]):
-    main_cells = ["{}{}".format('B', numb + 2) for numb in range(len(params_company[1:]))]
-    for num, cell in enumerate(main_cells):
-        ws[cell] = ans[params_company[num+1]]
 
-path = "for_task_1_simple.csv"
-with open(path, "w", newline='') as for_task_1_simple:
+for n, param in enumerate(PARAMS_COMPANY[1:]):
+    main_cells = ["{}{}".format('B', numb+2) 
+		for numb in range(len(PARAMS_COMPANY[1:]))]
+    for num, cell in enumerate(main_cells):
+        ws[cell] = ans[PARAMS_COMPANY[num+1]]
+
+		
+PATH = "for_task_1_simple.csv"
+
+with open(PATH, "w", newline='') as for_task_1_simple:
     write = writer(for_task_1_simple, delimiter=",")
-    write.writerow([params_company[0],''])
-    for line in range(len(params_company[:-1])):
-        write.writerow([params_company[line+1], ans[params_company[line+1]]])
+    write.writerow([PARAMS_COMPANY[0],''])
+    for line in range(len(PARAMS_COMPANY[:-1])):
+        write.writerow([PARAMS_COMPANY[line+1],
+					    ans[PARAMS_COMPANY[line+1]]])
 
 wb.save("for_task_1_simple.xlsx")
 
