54ce2d7ccac4c9fc42e1e9ddcbc68e6d905e2944
Maxim <max28.ozymand@gmail.com>
2020-04-07 12:10:37 +0300

    Task_1 version with a lot of company(input)

--- complic_task_1.py
+++ complic_task_1.py
@@ -1,15 +1,16 @@
-from bs4 import BeautifulSoup as Bs
-import requests
 import re
 import string as st
+from random import choice
+from csv import writer
+
+import requests
+from bs4 import BeautifulSoup as Bs
+from openpyxl import Workbook
 
 from for_task_1 import *
 
-from random import choice
-from openpyxl import Workbook
-from csv import writer
 
-params_company[0] = "№"
+PARAMS_COMPANY[0] = "№"
 companies = []
 while True:
     inp_url = input().lower()
@@ -19,34 +20,39 @@ while True:
     if inp_url.lower() == 'stop':
         break
 
-head_cells = ["{}{}".format(st.ascii_uppercase[numb], 1) for numb in range(len(params_company))]
+HEAD_CELLS = ["{}{}".format(st.ascii_uppercase[numb], 1) 
+	for numb in range(len(PARAMS_COMPANY))]
 wb = Workbook()
 ws = wb.active
 
-for numb, cell in enumerate(head_cells):
-    ws[cell] = params_company[numb]
+for numb, cell in enumerate(HEAD_CELLS):
+    ws[cell] = PARAMS_COMPANY[numb]
 
-path = "for_task_1_complic.csv"
-with open(path, "w", newline='') as for_task_1_complic:
+	
+PATH = "for_task_1_complic.csv"
+with open(PATH, "w", newline='') as for_task_1_complic:
     write = writer(for_task_1_complic, delimiter=",")
-    write.writerow(params_company)
+    write.writerow(PARAMS_COMPANY)
     for numb, company in enumerate(companies):
         for i in range(20):
-            useragent = {'User-agent': choice(useragents)}
+            USERAGENT = {'User-agent': choice(USERAGENTS)}
             try:
-                req = requests.get('{}{}'.format(main_url, company), headers=useragent)
+                req = requests.get('{}{}'.format(main_url, company),
+								   headers=USERAGENT)
             except:
                 pass
             else:
                 break
         bs = Bs(req.text, "html.parser")
         ans = get_params(bs)
-        main_cells = ["{}{}".format(st.ascii_uppercase[num+1], numb+2) for num in range(len(params_company[1:]))]
-        ws["A"+str(numb+2)] = numb+1
+        main_cells = ["{}{}".format(st.ascii_uppercase[num+1], numb+2)
+			for num in range(len(PARAMS_COMPANY[1:]))]
+        ws["A" + str(numb+2)] = numb+1
         row_csv = [numb+1]
         for num, cell in enumerate(main_cells):
-            ws[cell] = ans[params_company[num + 1]]
-            row_csv.append(ans[params_company[num + 1]])
+            ws[cell] = ans[PARAMS_COMPANY[num+1]]
+            row_csv.append(ans[PARAMS_COMPANY[num+1]])
         write.writerow(row_csv)
 
+		
 wb.save("for_task_1_complic.xlsx")
